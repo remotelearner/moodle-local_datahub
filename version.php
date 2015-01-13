358e4feadf35c92d096212c45ff2b4d83df45635
Brent Boghosian <brent.boghosian@remote-learner.net>
2015-01-13 12:42:24 -0500

    ELIS-9030: Update datahub summary_logs & schedule table plugin column: 'rlip' => 'dh'
    Conflicts:
    	local/datahub/version.php

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015010400;
+$plugin->version = 2015010401;
 $plugin->requires = 2014111001.00; // Requires this Moodle version
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.8.1.0 (Build: 20150104)';
+$plugin->release = '2.8.2.1 (Build: 20150113)';
 $plugin->dependencies = array(
     'local_eliscore' => 2015010400
 );
