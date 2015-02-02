587a463a23945c4a1e3150f2e5a09a136d388176
Brent Boghosian <brent.boghosian@remote-learner.net>
2015-02-02 16:06:02 -0500

    ELIS-9056: Update version releases & build dates.

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2015010401;
 $plugin->requires = 2014111001.00; // Requires this Moodle version
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.8.2.1 (Build: 20150113)';
+$plugin->release = '2.8.3.0 (Build: 20150202)';
 $plugin->dependencies = array(
     'local_eliscore' => 2015010400
 );
