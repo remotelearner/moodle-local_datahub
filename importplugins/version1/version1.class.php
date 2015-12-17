a7dc3e1dad1d5f1f29b74b567a0422f68ed4ca61
Derek Henderson <derek.henderson@remote-learner.net>
2015-12-17 10:38:31 -0500

    ELIS-9336 - Corrected issue when updating with Datahub import could allow upper case usernames

--- importplugins/version1/version1.class.php
+++ importplugins/version1/version1.class.php
@@ -395,7 +395,7 @@ class rlip_importplugin_version1 extends rlip_importplugin_base {
                 $action = 'create';
             }
         }
-        if ($action == 'create' && isset($record->username)) {
+        if (isset($record->username)) {
             $record->username = core_text::strtolower($record->username);
         }
         return $action;
