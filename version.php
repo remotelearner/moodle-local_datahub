6f47873752d3bc1d348a440205ac2c8762ccd1f3
Brent Boghosian <brent.boghosian@remote-learner.net>
2016-12-06 00:33:01 -0500

    DATAHUB-1590: Remove local_elisprogram dependency.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016052301;
+$plugin->version = 2016052302;
 $plugin->requires = 2016052300.00;
 $plugin->component = 'local_datahub';
 $plugin->maturity = MATURITY_STABLE;
@@ -33,7 +33,3 @@ $plugin->release = '3.1.0.1 (Build: 20160823)';
 $plugin->dependencies = array(
     'local_eliscore' => 2016052300
 );
-
-if (file_exists(dirname(__FILE__).'/../elisprogram/lib/setup.php')) {
-    $plugin->dependencies['local_elisprogram'] = 2016052300;
-}
